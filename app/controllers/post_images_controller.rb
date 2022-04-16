756c723fada479c871739abcfd54cb516107829a
mnmnats <mnmnats0313@gmail.com>
2022-04-16 05:49:32 +0000

    [Finish]4-22

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,17 +1,21 @@
 class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
+    
   end
 
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
+    if @post_image.save
     redirect_to post_images_path
+  else 
+    render :new
+  end
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
